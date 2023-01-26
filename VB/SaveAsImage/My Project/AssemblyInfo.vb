210f0574c53d071f4edc71f90789f455596cacf3
Polina Fedorova <polina.fedorova@devexpress.com>
2023-01-26 18:42:02 +0400

    update target to 6

--- VB/SaveAsImage/My Project/AssemblyInfo.vb
+++ VB/SaveAsImage/My Project/AssemblyInfo.vb
@@ -4,18 +4,6 @@ Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 Imports System.Windows
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-<Assembly: AssemblyTitle("SaveAsImage")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("SaveAsImage")>
-<Assembly: AssemblyCopyright("Copyright ©  2012")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
@@ -33,16 +21,3 @@ Imports System.Windows
 
 <Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
