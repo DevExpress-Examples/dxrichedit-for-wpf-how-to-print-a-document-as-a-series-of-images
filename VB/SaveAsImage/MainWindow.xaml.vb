4587c849bf9eab6bad0b16b502fe4bd780aa36db
DevExpressExampleBot <fakeEmail>
2023-01-26 18:48:01 +0400

    Source auto update [skip ci]

--- VB/SaveAsImage/MainWindow.xaml.vb
+++ VB/SaveAsImage/MainWindow.xaml.vb
@@ -1,44 +1,40 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
-#Region "#usings"
+'#Region "#usings"
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraPrintingLinks
 Imports DevExpress.XtraRichEdit
-#End Region ' #usings
-Imports System.Windows.Forms
 
-
+'#End Region  ' #usings
 Namespace SaveAsImage
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
 
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			Me.Cursor = System.Windows.Input.Cursors.Wait
-'			#Region "#saveasimage"
-			Dim pcl As New PrintableComponentLinkBase(New PrintingSystemBase())
-			pcl.Component = (CType(richEditControl1, IRichEditControl)).InnerControl
-			pcl.CreateDocument(False)
-			Dim imgOptions As New ImageExportOptions()
-			imgOptions.ExportMode = ImageExportMode.DifferentFiles
-			imgOptions.Format = System.Drawing.Imaging.ImageFormat.Png
-			imgOptions.Resolution = 150
-			imgOptions.PageRange = "1,3-5"
-			pcl.ExportToImage("export.png", imgOptions)
-'			#End Region ' #saveasimage
-			Me.Cursor = System.Windows.Input.Cursors.Arrow
+        Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Cursor = Input.Cursors.Wait
+'#Region "#saveasimage"
+            Dim pcl As PrintableComponentLinkBase = New PrintableComponentLinkBase(New PrintingSystemBase())
+            pcl.Component = CType(Me.richEditControl1, IRichEditControl).InnerControl
+            pcl.CreateDocument(False)
+            Dim imgOptions As ImageExportOptions = New ImageExportOptions()
+            imgOptions.ExportMode = ImageExportMode.DifferentFiles
+            imgOptions.Format = System.Drawing.Imaging.ImageFormat.Png
+            imgOptions.Resolution = 150
+            imgOptions.PageRange = "1,3-5"
+            pcl.ExportToImage("export.png", imgOptions)
+'#End Region  ' #saveasimage
+            Cursor = Input.Cursors.Arrow
+        End Sub
 
-		End Sub
-
-		Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			richEditControl1.LoadDocument("test.docx")
-		End Sub
-	End Class
+        Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.richEditControl1.LoadDocument("test.docx")
+        End Sub
+    End Class
 End Namespace
